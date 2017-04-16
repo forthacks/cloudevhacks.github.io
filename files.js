69cdc2e6b0021ec9f061a71bbb02cefd874b87ce
Oliver Ni <oliver.ni@gmail.com>
2017-04-15 21:29:51 -0700

    Stylized Projects page, Files page, Create Project page

--- files.js
+++
@@ -1,9 +0,0 @@
-firebase.database().ref('users/' + user.uid + "/" + window.location.hash).once('value').then(function(snapshot) {
-  document.getElementById("projects").innerHTML = ""
-  for (var i in snapshot.val()) {
-    document.getElementById("projects").innerHTML += "<li><a href='project.html#"+i+"'>"+i+"</a></li>"
-  }
-});
-function newfile() {
-  window.location.href = "createFile.html"+window.location.hash
-}
--- projects.js
+++ projects.js
@@ -7,8 +7,9 @@ firebase.auth().onAuthStateChanged(function(user) {
     firebase.database().ref('users/' + user.uid).once('value').then(function(snapshot) {
       document.getElementById("projects").innerHTML = ""
       for (var i in snapshot.val()) {
-        document.getElementById("projects").innerHTML += "<li><a href='project.html#"+i+"'>"+i+"</a></li>"
+        document.getElementById("projects").innerHTML += "<a class='project' href='project.html#"+i+"'><input type='text' value='"+i+"' readonly></a><br>"
       }
+      document.getElementById("projects").removeChild(document.getElementById("projects").lastChild)
     });
   } else {
     window.location.href = "index.html";
