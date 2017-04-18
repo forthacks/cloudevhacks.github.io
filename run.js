bfd1b2f26aecd68e2281ca5b8d8ce07582eea2af
thegithubgeek <wfrankw9@gmail.com>
2017-04-18 16:01:35 -0700

    Stuff

--- run.js
+++ run.js
@@ -1,5 +1,25 @@
 const shell = require("shelljs")
-
+const sys = require('sys')
+const exec = require('child_process').exec;
 function run(){
+  switch(language){
+    case "python":
+      break;
+    case "javascript":
+      break;
+    case "java":
+      break;
+    case "ruby":
+      break;
+    case "c++":
+      break;
+    case "perl":
+      break;
+    default:
+      language = "javascript"
+  }
+}
 
+function dwl() {
+  exec("echo "+firepad.getText()+'">Files/'+window.location.hash.substring(1).split("#")[1], function(error, stdout, stderr) {console.log(error+stderr+stdout})
 }
