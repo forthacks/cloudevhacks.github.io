85db4e358bd4598117b31e484d536ee6090ed096
thegithubgeek <wfrankw9@gmail.com>
2017-04-18 18:28:08 -0700

    Stuff

--- run.js
+++ run.js
@@ -1,6 +1,9 @@
 const shell = require("shelljs")
-const sys = require('sys')
-const exec = require('child_process').exec;
+const util = require('util')
+const child_process = require('child_process')
+const exec = require('child_process').exec
+const spawn = require('child_process').spawn;
+const fs = require('fs');
 function run(){
   switch(language){
     case "python":
@@ -21,5 +24,12 @@ function run(){
 }
 
 function dwl() {
-  exec("echo "+firepad.getText()+'">Files/'+window.location.hash.substring(1).split("#")[1], function(error, stdout, stderr) {console.log(error+stderr+stdout})
+  var stream = fs.createWriteStream("/Files/"+window.location.hash.substring(1).split("#")[1].replace("|","."));
+  stream.once('open', function(fd) {
+    stream.write(firepad.getText());
+    stream.end();
+  });
+  // fs.writeFile("")
+  // spawn('echo', [firepad.getText()+'" > Files/'+window.location.hash.substring(1).split("#")[1].replace("|",".")])
+  // exec('echo "'+firepad.getText()+'" > Files/'+window.location.hash.substring(1).split("#")[1].replace("|","."), function(error, stdout, stderr) {console.log(error+stderr+stdout)})
 }
